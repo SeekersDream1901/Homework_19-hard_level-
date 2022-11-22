cb1afab8ecf30a72fd7699090ca71fa7f9be336a
SeekersDream1901 <nikolaigarshin2112@gmail.com>
2022-11-22 21:08:57 +0300

    Added user DATA and user services.

--- app/container.py
+++ app/container.py
@@ -1,10 +1,12 @@
 from app.dao.director import DirectorDAO
 from app.dao.genre import GenreDAO
 from app.dao.movie import MovieDAO
+from app.dao.user import UserDAO
 from app.database import db
 from app.services.director import DirectorService
 from app.services.genre import GenreService
 from app.services.movie import MovieService
+from app.services.user import UserService
 
 
 movie_DAO = MovieDAO(db.session)
@@ -15,3 +17,6 @@ director_service = DirectorService(director_DAO)
 
 genre_DAO = GenreDAO(db.session)
 genre_service = GenreService(genre_DAO)
+
+user_DAO = UserDAO(db.session)
+user_service = UserService(user_DAO)
